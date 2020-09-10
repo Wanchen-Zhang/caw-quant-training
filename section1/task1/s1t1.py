b25c15498987d668835ee7304b3c190fc9bb0bbd
Wanchen Zhang <60902256+Wanchen-Zhang@users.noreply.github.com>
2020-09-09 22:09:53 -0400

    section1task1

---
+++ section1/task1/s1t1.py
@@ -0,0 +1,24 @@
+# CAW
+# Section 1, Task 1
+import pandas as pd
+import requests
+import datetime
+url = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=BTC&tsym=USDT&e=binance&limit=2000&toTs{1585699200}'
+f = requests.get(url)
+ipdata = f.json()
+df = pd.DataFrame(ipdata['Data']['Data'])
+df = df.iloc[:, 0:-2]
+fmt = "%Y-%m-%d %H:%M:%S"
+df['datetime'] = df['time']
+#for i in range(len(df)):
+#    df['datetime'][i] = datetime.datetime.fromtimestamp(df['time'][i]/1000.).strftime(fmt)
+df['datetime'] = pd.to_datetime(df['time'],unit='s')
+df = df.rename(columns={'volumefrom': 'volume'})
+df = df.rename(columns={'volumeto': 'baseVolume'})
+df = df.sort_values(by = ['datetime'])
+df.to_csv("s1t1.csv")
+
+# Questions:
+# 1. At the end of the URL, when specifying the '&toTs{1585699200}', it should be '&toTs={1585699200}' according to the API documentation, but when I include the '='
+# it gives me an error message.
+# 2. Although the toTs is included in the url, the data is still retrieved from the current time, how to fix it..?
---
+++ section1/task1/section1task1.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 18:11:20 2020
+
+@author: wanchenzhang
+"""
+
+
+# Questions:
+# 1. The basic structure is the same as the s1t1.py but has an additional while loop 
+# to constantly retrieve data. It seems didn't work as desired... 
+import pandas as pd
+import requests
+import datetime
+import time
+# Section 1, Task 1
+# Write a function to download histohour data, parameters:
+# fsym: BTC, tsym: USDT, start_time="2017-04-01", end_time="2020-04-01", e='binance'
+# time-zone: UTC
+
+def get_data(fsym, tsym, start_time, end_time, e):
+    """ returns pandas DataFrame 
+    """
+    url = 'https://min-api.cryptocompare.com/data/v2/histohour?'
+    fmt = "%Y-%m-%d %H:%M:%S"
+    start_time = str(datetime.datetime.strptime(start_time, '%Y-%m-%d'))
+    start_time = int(time.mktime(time.strptime(start_time, fmt)))
+    end_time = str(datetime.datetime.strptime(end_time, '%Y-%m-%d'))
+    end_time = int(time.mktime(time.strptime(end_time, fmt)))
+    date = end_time
+    holder = []
+    # Retrieve the data backwards
+    while date>start_time:
+        # Modify the URL using the given parameters
+        url = url+"fsym="+fsym+"&tsym="+tsym+"&e="+e+'&limit=2000'+'toTs{'+str(date)+'}'
+        f = requests.get(url)
+        ipdata = f.json()
+        df = pd.DataFrame(ipdata['Data']['Data'])
+        df = df.iloc[:, 0:-2]
+        df['datetime'] = df['time']
+        df = df.rename(columns={'volumefrom': 'volume'})
+        df = df.rename(columns={'volumeto': 'baseVolume'})
+        #for i in range(len(df)):
+            #df['datetime'][i] = datetime.datetime.utcfromtimestamp(df['datetime'][i]).strftime(fmt)
+        df['datetime'] = pd.to_datetime(df['time'],unit='s')
+        print(df)
+        holder.append(df)
+        # Update the date
+        date = df['datetime'][0]
+    
+    # sort the holder by date ascendingly
+    holder = holder.sort_values(by = ['datetime'])
+    return holder
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+# Use the __main__ section for all of your test cases. 
+# This section will automatically be executed when the file is run in Python
+if __name__ == '__main__':
+    print(get_data('BTC','USDT', "2017-04-01","2020-04-01",'binance'))
